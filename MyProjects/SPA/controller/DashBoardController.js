abc7670046099dff858edb1f2de9010f88603085
Kavishka <kavishkaprabath21@gmail.com>
2022-03-12 20:55:46 +0530

    Order content

---
+++ MyProjects/SPA/controller/DashBoardController.js
@@ -0,0 +1,31 @@
+/**
+ * @ author : Kavishka Prabath
+ * @ since : 0.1.0
+ **/
+
+// hide all content
+
+$("#customerContent").css("display","block");
+$("#itemContent").css("display","none");
+$("#orderContent").css("display","none");
+
+$("#linkCustomer").click(function (){
+    $("#customerContent").css("display","block");
+    $("#itemContent").css("display","none");
+    $("#orderContent").css("display","none");
+    loadAllCustomer();
+
+});
+
+$("#linkItem").click(function (){
+    $("#itemContent").css("display","block");
+    $("#customerContent").css("display","none");
+    $("#orderContent").css("display","none");
+});
+
+$("#linkPlaceOder").click(function (){
+    $("#orderContent").css("display","block");
+    $("#itemContent").css("display","none");
+    $("#customerContent").css("display","none");
+
+});
